178879634426fa88a1e912ddf7f1856ceab6359b
mikael <mikael.lazarev@gmail.com>
2022-05-10 13:51:07 +0200

    npm update

--- lib/core/token.d.ts
+++ lib/core/token.d.ts
@@ -19,9 +19,23 @@ export declare const priority: {
     7: number;
     6: number;
 };
-export interface TokenDataI {
-    type: TokenType;
-}
+export declare type TokenDataI = {
+    type: TokenType.CONNECTOR;
+} | {
+    type: TokenType.NORMAL_TOKEN;
+} | {
+    type: TokenType.CURVE_LP;
+} | {
+    type: TokenType.YEARN_VAULT;
+} | {
+    type: TokenType.META_CURVE_LP;
+} | {
+    type: TokenType.YEARN_VAULT_OF_CURVE_LP;
+} | {
+    type: TokenType.CONVEX_PHANTOM;
+} | {
+    type: TokenType.YEARN_VAULT_OF_META_CURVE_LP;
+};
 export declare type SupportedTokens = "1INCH" | "AAVE" | "COMP" | "CRV" | "DAI" | "DPI" | "FEI" | "LINK" | "SNX" | "SUSHI" | "UNI" | "USDC" | "USDT" | "WBTC" | "WETH" | "YFI" | "STETH" | "FTM" | "CVX" | "FRAX" | "FXS" | "LDO" | "SPELL" | "LUSD" | "sUSD" | "GUSD" | "LUNA" | "LQTY" | "yvDAI" | "yvUSDC" | "yvWETH" | "yvWBTC" | "3Crv" | "steCRV" | "FRAX3CRV" | "LUSD3CRV" | "crvPlain3andSUSD" | "gusd3CRV" | "cvx3Crv" | "cvxsteCRV" | "cvxFRAX3CRV" | "cvxcrvPlain3andSUSD" | "cvxgusd3CRV" | "yvCurve_stETH" | "yvCurve_FRAX";
 export declare const supportedTokens: Record<SupportedTokens, TokenDataI>;
 export declare const tokenDataByNetwork: Record<NetworkType, Record<SupportedTokens, string>>;
