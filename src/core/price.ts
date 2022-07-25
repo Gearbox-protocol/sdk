3d6f02191d295693fbde3b053ab54643ae429189
esser <blizzz93@gmail.com>
2022-07-25 14:16:10 +0900

    totalPrice has 18 decimals now

--- src/core/price.ts
+++ src/core/price.ts
@@ -1,9 +1,14 @@
 import { BigNumber } from "ethers";
+import { WAD, PRICE_DECIMALS } from "../core/constants";
 
 export const calcTotalPrice = (
   price: BigNumber,
   amount: BigNumber,
   decimals: number = 18
 ) => {
-  return amount.mul(price).div(BigNumber.from(10).pow(decimals));
+  return amount
+    .mul(WAD)
+    .mul(price)
+    .div(BigNumber.from(10).pow(decimals))
+    .div(PRICE_DECIMALS);
 };
