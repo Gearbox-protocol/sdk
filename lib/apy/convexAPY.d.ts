e09dd61499f1bbbcdeac024818ff4398ecc74308
esser <blizzz93@gmail.com>
2022-08-12 18:06:40 +0900

    mapping removed

--- lib/apy/convexAPY.d.ts
+++ lib/apy/convexAPY.d.ts
@@ -2,7 +2,5 @@ import { BigNumber, providers } from "ethers";
 import { ConvexPoolContract } from "../contracts/contracts";
 import { CurveLPToken } from "../tokens/curveLP";
 import { NetworkType } from "../core/constants";
-declare type SupportedPools = ConvexPoolContract;
-export declare function getConvexApy(pool: SupportedPools, provider: providers.Provider, networkType: NetworkType, getTokenPrice: (tokenAddress: string) => BigNumber): Promise<BigNumber>;
+export declare function getConvexApy(pool: ConvexPoolContract, provider: providers.Provider, networkType: NetworkType, getTokenPrice: (tokenAddress: string) => BigNumber): Promise<BigNumber>;
 export declare function getCurveBaseApy(curveLPToken: CurveLPToken): Promise<BigNumber>;
-export {};
