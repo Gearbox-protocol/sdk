658a61404623d0bfe8c97ef28bd6986c64bca8e5
esser <blizzz93@gmail.com>
2022-09-15 17:33:48 +0900

    fix: formatPercentage decimals

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -151,8 +151,8 @@ export function formatDate(date: Date): string {
   return [year, month, day].join("-");
 }
 
-export function formatPercentage(healthFactor: number): string {
-  return (healthFactor / PERCENTAGE_FACTOR).toFixed(2);
+export function formatPercentage(healthFactor: number, decimals = 2): string {
+  return (healthFactor / PERCENTAGE_FACTOR).toFixed(decimals);
 }
 
 export function formatLeverage(leverage: number, decimals = 2) {
