7b619e3544ec49584bd95ec7b29e69ae032887a7
Mikael <mikael.lazarev@gmail.com>
2022-09-19 12:42:25 +0200

    fix: adding utlis/network to index.ts

--- src/index.ts
+++ src/index.ts
@@ -59,6 +59,7 @@ export * from "./utils/errors";
 export { getPoolTokens, getUnderlyingToken } from "./utils/extracter";
 export { keyToLowercase, objectEntries, swapKeyValue } from "./utils/mappers";
 export * from "./utils/multicall";
+export * from "./utils/network";
 export * from "./utils/price";
 export { callRepeater } from "./utils/repeater";
 export * from "./utils/types";
