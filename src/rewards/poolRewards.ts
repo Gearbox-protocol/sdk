21b75de8c27ed3ff5f7b4df012b06b540fdbd151
0xMikko.eth <mikael.lazarev@gmail.com>
2022-10-23 01:22:54 +0200

    fix: computeAllRewards is static

--- src/rewards/poolRewards.ts
+++ src/rewards/poolRewards.ts
@@ -64,7 +64,7 @@ export class PoolRewards {
     );
   }
 
-  async computeAllRewards(
+  static async computeAllRewards(
     dieselToken: string,
     provider: providers.Provider,
     networkType: NetworkType,
