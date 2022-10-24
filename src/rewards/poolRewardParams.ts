1e5ac5f51ead34bf7a0e7d54b086467e1c613764
apeir99n <ilgiz.gimaltdinov@gmail.com>
2022-10-25 03:25:27 +0400

    updaated LM rewards

--- src/rewards/poolRewardParams.ts
+++ src/rewards/poolRewardParams.ts
@@ -45,4 +45,25 @@ poolRewardsPerBlock.Goerli.dwstETH.addValue(
   BigNumber.from(10).pow(18).mul(1636).div(100),
 );
 
+poolRewardsPerBlock.Mainnet.dDAI.addValue(
+  7694030,
+  BigNumber.from(10).pow(18).mul(2283).div(100),
+);
+poolRewardsPerBlock.Mainnet.dUSDC.addValue(
+  7694030,
+  BigNumber.from(10).pow(6).mul(2283).div(100),
+);
+poolRewardsPerBlock.Mainnet.dWETH.addValue(
+  7694030,
+  BigNumber.from(10).pow(18).mul(3196).div(100),
+);
+poolRewardsPerBlock.Mainnet.dWBTC.addValue(
+  7694030,
+  BigNumber.from(10).pow(8).mul(913).div(100),
+);
+poolRewardsPerBlock.Mainnet.dwstETH.addValue(
+  7694030,
+  BigNumber.from(10).pow(18).mul(1636).div(100),
+);
+
 // poolRewardsPerBlock.Goerli.dDAI.addValue(90000, BigNumber.from(0));
