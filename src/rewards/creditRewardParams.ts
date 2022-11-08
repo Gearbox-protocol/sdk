dd317ba17add4e6f75c70b95ce200d75da798dea
Mikael <mikael.lazarev@gmail.com>
2022-11-08 10:06:37 +0100

    fix: creditRewards for testing

--- src/rewards/creditRewardParams.ts
+++ src/rewards/creditRewardParams.ts
@@ -32,11 +32,11 @@ export const creditRewardsPerBlock: Record<CreditManagersV2, RangedValue> = {
 };
 
 export const GEAR_PER_BLOCK: Record<DieselTokenTypes, number> = {
-  dDAI: 2283,
-  dUSDC: 2283,
-  dWETH: 3196,
-  dWBTC: 913,
-  dwstETH: 1636,
+  dDAI: 166,
+  dUSDC: 166,
+  dWETH: 230,
+  dWBTC: 118,
+  dwstETH: 66,
 };
 
 export const GOERLI_BLOCK = 7694030;
@@ -59,5 +59,28 @@ creditRewardsPerBlock[CREDIT_MANAGER_WSTETH_V2_GOERLI].addValue(
 );
 creditRewardsPerBlock[CREDIT_MANAGER_WBTC_V2_GOERLI].addValue(
   GOERLI_BLOCK,
+  BigNumber.from(10).pow(18).mul(GEAR_PER_BLOCK.dWBTC).div(100),
+);
+
+export const MAINNET_BLOCK = 15724000;
+
+creditRewardsPerBlock[CREDIT_MANAGER_DAI_V2_MAINNET].addValue(
+  MAINNET_BLOCK,
+  BigNumber.from(10).pow(18).mul(GEAR_PER_BLOCK.dDAI).div(100),
+);
+creditRewardsPerBlock[CREDIT_MANAGER_USDC_V2_MAINNET].addValue(
+  MAINNET_BLOCK,
+  BigNumber.from(10).pow(18).mul(GEAR_PER_BLOCK.dUSDC).div(100),
+);
+creditRewardsPerBlock[CREDIT_MANAGER_WETH_V2_MAINNET].addValue(
+  MAINNET_BLOCK,
+  BigNumber.from(10).pow(18).mul(GEAR_PER_BLOCK.dWETH).div(100),
+);
+creditRewardsPerBlock[CREDIT_MANAGER_WSTETH_V2_MAINNET].addValue(
+  MAINNET_BLOCK,
+  BigNumber.from(10).pow(18).mul(GEAR_PER_BLOCK.dwstETH).div(100),
+);
+creditRewardsPerBlock[CREDIT_MANAGER_WBTC_V2_MAINNET].addValue(
+  MAINNET_BLOCK,
   BigNumber.from(10).pow(18).mul(GEAR_PER_BLOCK.dWBTC).div(100),
 );
