4435cffca56407cac292d0fc119f2b20da8b2ed2
esser <blizzz93@gmail.com>
2022-12-21 09:48:12 +0800

    fix: unused text errors removed

--- src/core/errors.ts
+++ src/core/errors.ts
@@ -5,47 +5,6 @@ export interface MetamaskError {
   message: string;
   data: string;
 }
-
-export class NetworkError extends Error {
-  constructor() {
-    super("errors.networkError");
-  }
-}
-export class IncorrectEthAddressError extends Error {
-  constructor() {
-    super("errors.incorrectEthAddressError");
-  }
-}
-
-export class PoolNotExistsError extends Error {
-  constructor() {
-    super("errors.poolDoesntExistsError");
-  }
-}
-
-export class CreditManagerNotExistsError extends Error {
-  constructor() {
-    super("errors.creditManagerDoesntExistsError");
-  }
-}
-export class UserHasNotAccountError extends Error {
-  constructor() {
-    super("errors.noOpenedAccountsError");
-  }
-}
-
-export class PathNotFoundError extends Error {
-  constructor() {
-    super("errors.pathNotFoundError");
-  }
-}
-
-export class AccountsInAllCreditManagersError extends Error {
-  constructor() {
-    super("errors.youOpenedAccountsInAllCreditManagersError");
-  }
-}
-
 export type OpenAccountErrorTypes =
   | "insufficientPoolLiquidity"
   | "leverageGreaterMax"
