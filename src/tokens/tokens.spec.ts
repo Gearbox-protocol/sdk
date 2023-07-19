c9e2c40655fe58fcdb9928c890dc00a283d39eca
doomsower <K.Kuznetcov@gmail.com>
2023-07-19 11:46:16 -0400

    test: test token symbols

---
+++ src/tokens/tokens.spec.ts
@@ -0,0 +1,34 @@
+import { expect } from "chai";
+import { ethers } from "ethers";
+
+import { IERC20Metadata__factory } from "../types";
+import { IERC20MetadataInterface } from "../types/@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata";
+import { MCall, multicall } from "../utils/multicall";
+import { tokenDataByNetwork } from "./token";
+
+describe("Tokens tests", () => {
+  it("token symbols should match onchain symbols", async () => {
+    const provider = new ethers.providers.StaticJsonRpcProvider(
+      process.env.MAINNET_TESTS_FORK,
+    );
+    const entries = Object.entries(tokenDataByNetwork.Mainnet);
+    const erc20 = IERC20Metadata__factory.createInterface();
+
+    const mcalls: MCall<IERC20MetadataInterface>[] = entries.map(
+      ([, address]) => ({
+        address,
+        interface: erc20,
+        method: "symbol()",
+      }),
+    );
+
+    const onchainSymbols = await multicall<string[]>(mcalls, provider);
+    const onchainMap: Record<string, string> = {};
+
+    for (let i = 0; i < entries.length; i++) {
+      onchainMap[onchainSymbols[i]] = entries[i][1];
+    }
+
+    expect(onchainMap).to.be.equal(tokenDataByNetwork.Mainnet);
+  });
+});
