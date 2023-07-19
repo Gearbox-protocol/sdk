9114d2745f8888435fff43f10fc82fd388e39e2b
Maxim Manylov <manylov@gmail.com>
2023-07-19 14:45:10 +0600

    feat: add utils contracts

--- src/contracts/utilsContracts.ts
+++ src/contracts/utilsContracts.ts
@@ -20,3 +20,8 @@ export const CREATE2FACTORY: Record<NetworkType, string> = {
   Mainnet: "0x59b7B8Dd9E6e1F934C9c3Def4a1Eb69Bc17Ec9cc",
   Arbitrum: NOT_DEPLOYED,
 };
+
+export const MULTISIG: Record<NetworkType, string> = {
+  Mainnet: "0xA7D5DDc1b8557914F158076b228AA91eF613f1D5",
+  Arbitrum: NOT_DEPLOYED,
+};
