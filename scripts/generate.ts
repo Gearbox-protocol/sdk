536ece9edaef908de7597783be4784f0a0d6911e
Mikael <26343374+0xmikko@users.noreply.github.com>
2023-07-28 22:59:44 +0200

    fix: test luna token

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -28,7 +28,7 @@ class BindingsGenerator {
 
   generateTokens() {
     const tokensEnum = this.tokens.map(t => this.safeEnum(t)).join(",\n");
-    let data = `enum Tokens {NO_TOKEN, ${tokensEnum}}`;
+    let data = `enum Tokens {NO_TOKEN, LUNA, ${tokensEnum}}`;
 
     const tokenTypeEnum = Object.values(TokenType)
       .filter(v => isNaN(Number(v)))
