310d9737db20bce4c9894bb6ddccc9b6bc48bd57
esser <blizzz93@gmail.com>
2023-07-29 11:20:44 +0900

    fix: better pools and cms list

--- src/contracts/contractsRegister.ts
+++ src/contracts/contractsRegister.ts
@@ -7,53 +7,105 @@ export const stEthPoolWrapper: Record<NetworkType, string> = {
   Arbitrum: "0x3fe62a62C022A069952069b32C9d56718D30B7ec",
 };
 
-export const CREDIT_MANAGER_DAI_V2_MAINNET =
-  "0x672461Bfc20DD783444a830Ad4c38b345aB6E2f7".toLowerCase();
+type MainnetCreditManagersV1 = "DAI_V1" | "USDC_V1" | "WETH_V1" | "WBTC_V1";
+type MainnetCreditManagersV2 =
+  | "DAI_V2"
+  | "USDC_V2"
+  | "WETH_V2"
+  | "WSTETH_V2"
+  | "WBTC_V2"
+  | "FRAX_V2";
+type MainnetCreditManagersV2_1 = "WETH_V2_1";
 
-export const CREDIT_MANAGER_USDC_V2_MAINNET =
-  "0x95357303f995e184A7998dA6C6eA35cC728A1900".toLowerCase();
+export type MainnetCreditManagers =
+  | MainnetCreditManagersV1
+  | MainnetCreditManagersV2
+  | MainnetCreditManagersV2_1;
 
-export const CREDIT_MANAGER_WETH_V2_MAINNET =
-  "0x5887ad4Cb2352E7F01527035fAa3AE0Ef2cE2b9B".toLowerCase();
+export type ArbitrumCreditManagers = never;
 
-export const CREDIT_MANAGER_WSTETH_V2_MAINNET =
-  "0xe0bCE4460795281d39c91da9B0275BcA968293de".toLowerCase();
+type CreditManagersListType = {
+  [key in NetworkType]: key extends "Mainnet"
+    ? Record<MainnetCreditManagers, string>
+    : key extends "Arbitrum"
+    ? Record<ArbitrumCreditManagers, string>
+    : never;
+};
 
-export const CREDIT_MANAGER_WBTC_V2_MAINNET =
-  "0xc62BF8a7889AdF1c5Dc4665486c7683ae6E74e0F".toLowerCase();
+export const creditManagerByNetwork: CreditManagersListType = {
+  Mainnet: {
+    DAI_V1: "0x777E23A2AcB2fCbB35f6ccF98272d03C722Ba6EB".toLowerCase(),
+    USDC_V1: "0x2664cc24CBAd28749B3Dd6fC97A6B402484De527".toLowerCase(),
+    WETH_V1: "0x968f9a68a98819E2e6Bb910466e191A7b6cf02F0".toLowerCase(),
+    WBTC_V1: "0xC38478B0A4bAFE964C3526EEFF534d70E1E09017".toLowerCase(),
 
-export const CREDIT_MANAGER_FRAX_V2_MAINNET =
-  "0xA3E1e0d58FE8dD8C9dd48204699a1178f1B274D8".toLowerCase();
+    DAI_V2: "0x672461Bfc20DD783444a830Ad4c38b345aB6E2f7".toLowerCase(),
+    USDC_V2: "0x95357303f995e184A7998dA6C6eA35cC728A1900".toLowerCase(),
+    WETH_V2: "0x5887ad4Cb2352E7F01527035fAa3AE0Ef2cE2b9B".toLowerCase(),
+    WSTETH_V2: "0xe0bCE4460795281d39c91da9B0275BcA968293de".toLowerCase(),
+    WBTC_V2: "0xc62BF8a7889AdF1c5Dc4665486c7683ae6E74e0F".toLowerCase(),
+    FRAX_V2: "0xA3E1e0d58FE8dD8C9dd48204699a1178f1B274D8".toLowerCase(),
 
-export type CreditManagersV2 =
-  | typeof CREDIT_MANAGER_DAI_V2_MAINNET
-  | typeof CREDIT_MANAGER_USDC_V2_MAINNET
-  | typeof CREDIT_MANAGER_WETH_V2_MAINNET
-  | typeof CREDIT_MANAGER_WSTETH_V2_MAINNET
-  | typeof CREDIT_MANAGER_WBTC_V2_MAINNET
-  | typeof CREDIT_MANAGER_FRAX_V2_MAINNET;
-export const deployedContracts: Record<string, string> = {
-  // MAINNET
-  "0x777E23A2AcB2fCbB35f6ccF98272d03C722Ba6EB": "DAI",
-  "0x2664cc24CBAd28749B3Dd6fC97A6B402484De527": "USDC",
-  "0x968f9a68a98819E2e6Bb910466e191A7b6cf02F0": "WETH",
-  "0xC38478B0A4bAFE964C3526EEFF534d70E1E09017": "WBTC",
-  "0x79012c8d491dcf3a30db20d1f449b14caf01da6c": "FRAX",
+    WETH_V2_1: "0x4C6309fe2085EfE7A0Cfb426C16Ef3b41198cCE3".toLowerCase(),
+  },
+  Arbitrum: {},
+};
 
-  // MAINNET POOLS
-  "0x24946bCbBd028D5ABb62ad9B635EB1b1a67AF668": "DAI",
-  "0x86130bDD69143D8a4E5fc50bf4323D48049E98E4": "USDC",
-  "0xB03670c20F87f2169A7c4eBE35746007e9575901": "WETH",
-  "0xB8cf3Ed326bB0E51454361Fb37E9E8df6DC5C286": "wstETH",
-  "0xB2A015c71c17bCAC6af36645DEad8c572bA08A08": "WBTC",
+export type MainnetPools =
+  | "DAI_V1"
+  | "USDC_V1"
+  | "WETH_V1"
+  | "WBTC_V1"
+  | "WSTETH_V1"
+  | "FRAX_V1";
+
+export type ArbitrumPools = never;
+
+type PoolsListType = {
+  [key in NetworkType]: key extends "Mainnet"
+    ? Record<MainnetPools, string>
+    : key extends "Arbitrum"
+    ? Record<ArbitrumPools, string>
+    : never;
+};
+
+export const poolByNetwork: PoolsListType = {
+  Mainnet: {
+    DAI_V1: "0x24946bCbBd028D5ABb62ad9B635EB1b1a67AF668".toLowerCase(),
+    USDC_V1: "0x86130bDD69143D8a4E5fc50bf4323D48049E98E4".toLowerCase(),
+    WETH_V1: "0xB03670c20F87f2169A7c4eBE35746007e9575901".toLowerCase(),
+    WBTC_V1: "0xB2A015c71c17bCAC6af36645DEad8c572bA08A08".toLowerCase(),
+    WSTETH_V1: "0xB8cf3Ed326bB0E51454361Fb37E9E8df6DC5C286".toLowerCase(),
+    FRAX_V1: "0x79012c8d491dcf3a30db20d1f449b14caf01da6c".toLowerCase(),
+  },
+  Arbitrum: {},
+};
+
+const deployedContractNames: Record<string, string> = {
+  // MAINNET V1 CM
+  [creditManagerByNetwork.Mainnet.DAI_V1]: "DAI",
+  [creditManagerByNetwork.Mainnet.USDC_V1]: "USDC",
+  [creditManagerByNetwork.Mainnet.WETH_V1]: "WETH",
+  [creditManagerByNetwork.Mainnet.WBTC_V1]: "WBTC",
 
   // MAINNET V2 CM
-  [CREDIT_MANAGER_DAI_V2_MAINNET]: "DAI",
-  [CREDIT_MANAGER_USDC_V2_MAINNET]: "USDC",
-  [CREDIT_MANAGER_WETH_V2_MAINNET]: "WETH",
-  [CREDIT_MANAGER_WSTETH_V2_MAINNET]: "wstETH",
-  [CREDIT_MANAGER_WBTC_V2_MAINNET]: "WBTC",
-  [CREDIT_MANAGER_FRAX_V2_MAINNET]: "FRAX",
+  [creditManagerByNetwork.Mainnet.DAI_V2]: "DAI",
+  [creditManagerByNetwork.Mainnet.USDC_V2]: "USDC",
+  [creditManagerByNetwork.Mainnet.WETH_V2]: "WETH",
+  [creditManagerByNetwork.Mainnet.WSTETH_V2]: "wstETH",
+  [creditManagerByNetwork.Mainnet.WBTC_V2]: "WBTC",
+  [creditManagerByNetwork.Mainnet.FRAX_V2]: "FRAX",
+
+  // MAINNET V2_1 CM
+  [creditManagerByNetwork.Mainnet.WETH_V2_1]: "WETH-Alpha",
+
+  // MAINNET POOLS
+  [poolByNetwork.Mainnet.DAI_V1]: "DAI",
+  [poolByNetwork.Mainnet.USDC_V1]: "USDC",
+  [poolByNetwork.Mainnet.WETH_V1]: "WETH",
+  [poolByNetwork.Mainnet.WBTC_V1]: "WBTC",
+  [poolByNetwork.Mainnet.WSTETH_V1]: "wstETH",
+  [poolByNetwork.Mainnet.FRAX_V1]: "FRAX",
 };
 
 const contractNames = Object.entries(contractsByAddress).reduce<
@@ -66,7 +118,7 @@ const contractNames = Object.entries(contractsByAddress).reduce<
 }, {});
 
 const contractsFullList = {
-  ...TypedObjectUtils.keyToLowercase(deployedContracts),
+  ...TypedObjectUtils.keyToLowercase(deployedContractNames),
   ...contractNames,
 };
 
