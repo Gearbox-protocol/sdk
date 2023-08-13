d3e0ff7b51625aaab626c2e56c802e6e1e07b76b
esser <blizzz93@gmail.com>
2023-08-13 21:59:02 +0800

    fix: add test

--- src/pathfinder/pathfinder.spec.ts
+++ src/pathfinder/pathfinder.spec.ts
@@ -1,6 +1,8 @@
 import { expect } from "chai";
+import { providers } from "ethers";
 
 import { RAY } from "../core/constants";
+import { tokenDataByNetwork } from "../tokens/token";
 import { CloseResult, PathFinder } from "./pathfinder";
 
 describe("PathFinder test", () => {
@@ -22,5 +24,20 @@ describe("PathFinder test", () => {
 
     result = PathFinder.compare(r1, r2, RAY * 2n);
     expect(result).to.be.eql(r1);
+  });
+  it("has all expected connectors", () => {
+    const pf = new PathFinder("", new providers.JsonRpcProvider(), "Mainnet");
+    const allowedTokens = {
+      [tokenDataByNetwork.Mainnet.USDC]: true,
+      [tokenDataByNetwork.Mainnet.WETH]: true,
+      [tokenDataByNetwork.Mainnet.DAI]: true,
+      [tokenDataByNetwork.Mainnet.FRAX]: true,
+    } as const;
+    expect(pf.getAvailableConnectors(allowedTokens)).to.be.deep.equal([
+      tokenDataByNetwork.Mainnet.USDC,
+      tokenDataByNetwork.Mainnet.WETH,
+      tokenDataByNetwork.Mainnet.DAI,
+      tokenDataByNetwork.Mainnet.FRAX,
+    ]);
   });
 });
