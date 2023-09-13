b02e004483f144a216d88b0bc1a3728d2e517a48
esser <blizzz93@gmail.com>
2023-09-13 11:04:52 +0800

    fix: multiple data compressors parsing

--- src/parsers/dataCompressorParser.ts
+++ src/parsers/dataCompressorParser.ts
@@ -1,11 +1,28 @@
 import { IDataCompressor__factory } from "../types";
+import {
+  IDataCompressorV2_10__factory,
+  IDataCompressorV3_00__factory,
+} from "../types-v3";
 import { AbstractParser } from "./abstractParser";
 import { IParser } from "./iParser";
 
 export class DataCompressorParser extends AbstractParser implements IParser {
-  constructor() {
+  constructor(version: number) {
     super("DataCompressor");
-    this.ifc = IDataCompressor__factory.createInterface();
+
+    switch (version) {
+      case 2:
+        this.ifc = IDataCompressor__factory.createInterface();
+        break;
+      case 210:
+        this.ifc = IDataCompressorV2_10__factory.createInterface();
+        break;
+      case 300:
+        this.ifc = IDataCompressorV3_00__factory.createInterface();
+        break;
+      default:
+        this.ifc = IDataCompressor__factory.createInterface();
+    }
   }
   parse(calldata: string): string {
     const { functionFragment, functionName } = this.parseSelector(calldata);
