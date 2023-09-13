8ce06908d44cbaa757e1f435314335c7048cd636
esser <blizzz93@gmail.com>
2023-09-13 09:51:20 +0800

    fix: data compressor v3 and v2.1 added

---
+++ contracts/index.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+// Gearbox Protocol. Generalized leverage for DeFi protocols
+// (c) Gearbox Foundation, 2023.
+
+pragma solidity ^0.8.10;
+
+import {IDataCompressorV2_10} from "@gearbox-protocol/periphery-v3/contracts/interfaces/IDataCompressorV2_10.sol";
+import {IDataCompressorV3_00} from "@gearbox-protocol/periphery-v3/contracts/interfaces/IDataCompressorV3_00.sol";
