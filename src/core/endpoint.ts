0b3a90c408cd9f93bbd351583b7dd31f7c837a35
esser <blizzz93@gmail.com>
2023-12-12 17:25:39 +0800

    feat: add charts endpoint

---
+++ src/core/endpoint.ts
@@ -0,0 +1,40 @@
+import { CHAINS } from "@gearbox-protocol/sdk-gov";
+
+export const GEARBOX_RPC_CHAIN_ID = 7878;
+
+type ChartsPriceSource = "chainlink" | "spot";
+
+const CHARTS_BACKEND_ADDRESSES: Record<number, string> = {
+  [CHAINS.Mainnet]: "https://mainnet.gearbox.foundation",
+  [CHAINS.Local]: "https://mainnet.gearbox.foundation/v2",
+  [GEARBOX_RPC_CHAIN_ID]: "https://testnet.gearbox.foundation",
+};
+
+interface Options {
+  params?: Record<string, string | number>;
+}
+
+export class ChartsApi {
+  static getUrl = (
+    url: string,
+    chainId: number,
+    options?: Options,
+    priceSource?: ChartsPriceSource,
+  ) =>
+    [
+      CHARTS_BACKEND_ADDRESSES[chainId],
+      "api",
+      ...(priceSource ? [priceSource] : []),
+      this.getRelativeUrl(url, options),
+    ].join("/");
+
+  private static getRelativeUrl = (url: string, options?: Options) => {
+    const { params = {} } = options || {};
+
+    const paramsString = Object.entries(params)
+      .map<string>(([key, value]) => `${key}=${value}`)
+      .join("&");
+
+    return [url, ...(paramsString ? [paramsString] : [])].join("?");
+  };
+}
