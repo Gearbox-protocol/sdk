06719a6933fff3042e4d52497fcd067373979b69
doomsower <12031673+doomsower@users.noreply.github.com>
2024-09-30 09:12:14 -0300

    fix: exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
+export * from "./base";
+export * from "./chain";
+export * from "./constants";
+export * from "./core";
 export * from "./GearboxSDK";
+export * from "./market";
 export type * from "./SDKEvents";
+export * from "./state";
 export type * from "./types";
 export * from "./utils";
