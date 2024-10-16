3f4fd995961efb8ccfe3279a7d6a0fa6105f57cc
doomsower <12031673+doomsower@users.noreply.github.com>
2024-10-16 11:06:27 -0300

    fix: add mustFindBySymbol

--- src/sdk/base/TokensMeta.ts
+++ src/sdk/base/TokensMeta.ts
@@ -15,4 +15,12 @@ export class TokensMeta extends AddressMap<TokenMetaData> {
   public findBySymbol(symbol: string): TokenMetaData | undefined {
     return this.values().find(v => v.symbol === symbol);
   }
+
+  public mustFindBySymbol(symbol: string): TokenMetaData {
+    const meta = this.findBySymbol(symbol);
+    if (!meta) {
+      throw new Error(`cannot find token meta for symbol '${symbol}'`);
+    }
+    return meta;
+  }
 }
