fa833a08031667015a5201861efa720320a3f2d7
esser <blizzz93@gmail.com>
2024-10-25 13:58:20 +0800

    fix: return constants

--- src/apy/convexAPY.ts
+++ src/apy/convexAPY.ts
@@ -1,3 +1,9 @@
+import { WAD } from "@gearbox-protocol/sdk-gov";
+
+const CVX_MAX_SUPPLY = WAD * 100000000n;
+const CVX_REDUCTION_PER_CLIFF = 100000n;
+const CVX_TOTAL_CLIFFS = WAD * 1000n;
+
 export function getCVXMintAmount(crvAmount: bigint, cvxTokenSupply: bigint) {
   const currentCliff = cvxTokenSupply / CVX_REDUCTION_PER_CLIFF;
 
