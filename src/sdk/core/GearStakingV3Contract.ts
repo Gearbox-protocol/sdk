2be63fe31b78e0686db5aabdb8dc38c83c0b22d0
esser <blizzz93@gmail.com>
2024-11-12 19:45:47 +0800

    feat: gauge methods

--- src/sdk/core/GearStakingV3Contract.ts
+++ src/sdk/core/GearStakingV3Contract.ts
@@ -1,12 +1,20 @@
 import type { Address, DecodeFunctionDataReturnType } from "viem";
 
 import { gearStakingV3Abi } from "../abi";
+import { PermitResult } from "../accounts";
 import { BaseContract, VotingContractStatus } from "../base";
 import { ADDRESS_0X0 } from "../constants";
 import type { GearboxSDK } from "../GearboxSDK";
-import type { GearStakingV3StateHuman } from "../types";
+import type { GearStakingV3StateHuman, RawTx } from "../types";
 
 type abi = typeof gearStakingV3Abi;
+
+export interface MultiVote {
+  votingContract: Address;
+  voteAmount: bigint;
+  isIncrease: boolean;
+  extraData: Address;
+}
 
 export class GearStakingContract extends BaseContract<abi> {
   constructor(sdk: GearboxSDK, address: Address) {
@@ -32,5 +40,40 @@ export class GearStakingContract extends BaseContract<abi> {
       successor: this.labelAddress(ADDRESS_0X0),
       migrator: this.labelAddress(ADDRESS_0X0),
     };
+  }
+
+  public deposit(amount: bigint, votes: Array<MultiVote>): RawTx {
+    return this.createRawTx({
+      functionName: "deposit",
+      args: [amount, votes],
+    });
+  }
+  public depositWithPermit(
+    amount: bigint,
+    votes: Array<MultiVote>,
+    permit: PermitResult,
+  ): RawTx {
+    return this.createRawTx({
+      functionName: "depositWithPermit",
+      args: [amount, votes, permit.deadline, permit.v, permit.r, permit.s],
+    });
+  }
+  public withdraw(amount: bigint, to: Address, votes: Array<MultiVote>): RawTx {
+    return this.createRawTx({
+      functionName: "withdraw",
+      args: [amount, to, votes],
+    });
+  }
+  public claimWithdrawals(to: Address): RawTx {
+    return this.createRawTx({
+      functionName: "claimWithdrawals",
+      args: [to],
+    });
+  }
+  public multivote(votes: Array<MultiVote>): RawTx {
+    return this.createRawTx({
+      functionName: "multivote",
+      args: [votes],
+    });
   }
 }
