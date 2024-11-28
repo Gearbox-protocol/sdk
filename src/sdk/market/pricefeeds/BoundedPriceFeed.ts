3f48c15e38a96ecff803eb96cf2b9a7289a7aee2
doomsower <12031673+doomsower@users.noreply.github.com>
2024-11-28 10:23:50 -0300

    fix: loading upper bound from compressor

--- src/sdk/market/pricefeeds/BoundedPriceFeed.ts
+++ src/sdk/market/pricefeeds/BoundedPriceFeed.ts
@@ -1,3 +1,5 @@
+import { decodeAbiParameters } from "viem";
+
 import { boundedPriceFeedAbi } from "../../abi";
 import type { GearboxSDK } from "../../GearboxSDK";
 import type { BoundedOracleStateHuman } from "../../types";
@@ -7,10 +9,17 @@ import { AbstractPriceFeedContract } from "./AbstractPriceFeed";
 type abi = typeof boundedPriceFeedAbi;
 
 export class BoundedPriceFeedContract extends AbstractPriceFeedContract<abi> {
-  upperBound = 0n;
+  public readonly upperBound: bigint;
 
   constructor(sdk: GearboxSDK, args: PartialPriceFeedTreeNode) {
     super(sdk, { ...args, name: "BoundedPriceFeed", abi: boundedPriceFeedAbi });
+    const decoder = decodeAbiParameters(
+      [
+        { type: "int256" }, //  upperBound
+      ],
+      args.baseParams.serializedParams,
+    );
+    this.upperBound = decoder[0];
   }
 
   public override stateHuman(
