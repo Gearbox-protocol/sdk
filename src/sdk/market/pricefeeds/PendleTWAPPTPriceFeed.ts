3e5fd615de241899e72cb7d10b9e76e7b7a74d2c
doomsower <12031673+doomsower@users.noreply.github.com>
2024-12-17 13:19:55 -0300

    feat: decoding pendle price feed params

--- src/sdk/market/pricefeeds/PendleTWAPPTPriceFeed.ts
+++ src/sdk/market/pricefeeds/PendleTWAPPTPriceFeed.ts
@@ -1,3 +1,6 @@
+import type { Address } from "viem";
+import { decodeAbiParameters } from "viem";
+
 import { pendleTWAPPTPriceFeedAbi } from "../../abi";
 import type { GearboxSDK } from "../../GearboxSDK";
 import type { PartialPriceFeedTreeNode } from "./AbstractPriceFeed";
@@ -7,11 +10,35 @@ const abi = pendleTWAPPTPriceFeedAbi;
 type abi = typeof abi;
 
 export class PendleTWAPPTPriceFeed extends AbstractPriceFeedContract<abi> {
+  public readonly market: Address;
+  public readonly sy: Address;
+  public readonly yt: Address;
+  public readonly expiry: bigint;
+  public readonly twapWindow: number;
+  public readonly priceToSy: boolean;
+
   constructor(sdk: GearboxSDK, args: PartialPriceFeedTreeNode) {
     super(sdk, {
       ...args,
       name: "PendleTWAPPTPriceFeed",
       abi,
     });
+    const decoded = decodeAbiParameters(
+      [
+        { type: "address", name: "market" },
+        { type: "address", name: "sy" },
+        { type: "address", name: "yt" },
+        { type: "uint256", name: "expiry" },
+        { type: "uint32", name: "twapWindow" },
+        { type: "bool", name: "priceToSy" },
+      ],
+      args.baseParams.serializedParams,
+    );
+    this.market = decoded[0];
+    this.sy = decoded[1];
+    this.yt = decoded[2];
+    this.expiry = decoded[3];
+    this.twapWindow = decoded[4];
+    this.priceToSy = decoded[5];
   }
 }
