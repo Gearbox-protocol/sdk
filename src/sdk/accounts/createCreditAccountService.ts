54cde0e2796ae9d2f83638928143c2630ba6654a
esser <blizzz93@gmail.com>
2025-07-03 10:25:33 +0700

    fix: common type

--- src/sdk/accounts/createCreditAccountService.ts
+++ src/sdk/accounts/createCreditAccountService.ts
@@ -1,9 +1,8 @@
 import { isV300, isV310 } from "../constants/index.js";
 import type { GearboxSDK } from "../GearboxSDK.js";
-import type { AbstractCreditAccountService } from "./AbstractCreditAccountsService.js";
 import { CreditAccountServiceV300 } from "./CreditAccountsServiceV300.js";
 import { CreditAccountServiceV310 } from "./CreditAccountsServiceV310.js";
-import type { ICreditAccountsService } from "./types.js";
+import type { CreditAccountsServiceInstance } from "./types.js";
 
 /**
  * @sdk
@@ -13,7 +12,7 @@ import type { ICreditAccountsService } from "./types.js";
 export function createCreditAccountService(
   sdk: GearboxSDK,
   version: number,
-): AbstractCreditAccountService & ICreditAccountsService {
+): CreditAccountsServiceInstance {
   if (isV300(version)) {
     return new CreditAccountServiceV300(sdk);
   }
