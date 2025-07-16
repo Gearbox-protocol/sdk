595d6aee60754c987c2aa11a8949c047c74825ae
doomsower <12031673+doomsower@users.noreply.github.com>
2025-07-16 11:12:56 -0400

    fix: pass options to createCreditAccountService

--- src/sdk/accounts/createCreditAccountService.ts
+++ src/sdk/accounts/createCreditAccountService.ts
@@ -1,5 +1,6 @@
 import { isV300, isV310 } from "../constants/index.js";
 import type { GearboxSDK } from "../GearboxSDK.js";
+import type { CreditAccountServiceOptions } from "./AbstractCreditAccountsService.js";
 import { CreditAccountServiceV300 } from "./CreditAccountsServiceV300.js";
 import { CreditAccountServiceV310 } from "./CreditAccountsServiceV310.js";
 import type { CreditAccountsServiceInstance } from "./types.js";
@@ -12,12 +13,13 @@ import type { CreditAccountsServiceInstance } from "./types.js";
 export function createCreditAccountService(
   sdk: GearboxSDK,
   version: number,
+  options?: CreditAccountServiceOptions,
 ): CreditAccountsServiceInstance {
   if (isV300(version)) {
-    return new CreditAccountServiceV300(sdk);
+    return new CreditAccountServiceV300(sdk, options);
   }
   if (isV310(version)) {
-    return new CreditAccountServiceV310(sdk);
+    return new CreditAccountServiceV310(sdk, options);
   }
   throw new Error(`Unsupported Credit Account Service version ${version}`);
 }
