7caddd4ed6ff9f083d6a14f4b58e3ffbc8bf9ddf
doomsower <12031673+doomsower@users.noreply.github.com>
2025-08-05 16:19:29 -0400

    fix: attempt to fix zod address bug

--- src/sdk/utils/zod.ts
+++ src/sdk/utils/zod.ts
@@ -1,11 +1,14 @@
-import { getAddress, isAddress } from "viem";
+import { type Address, getAddress, isAddress } from "viem";
 import { z } from "zod/v4";
 
 /**
  * Like Address from abitype/zod, but converts an address into an address that is checksum encoded.
  */
-export const ZodAddress = () =>
-  z.string().transform((val, ctx) => {
+export const ZodAddress = (): z.ZodPipe<
+  z.ZodString,
+  z.ZodTransform<Address, string>
+> =>
+  z.string().transform((val, ctx): Address => {
     if (!isAddress(val)) {
       ctx.issues.push({
         code: "custom",
