31d0bb0bae5a46d504716f02b6e7048c5dd5b432
esser <blizzz93@gmail.com>
2025-08-16 08:15:18 +0700

    Revert "feat: add tulipa MC to default (#356)"
    This reverts commit c3e211adf816fb5caad946a43fc94796bddc87f8.

--- src/sdk/chain/chains.ts
+++ src/sdk/chain/chains.ts
@@ -99,9 +99,10 @@ export const chains: Record<NetworkType, GearboxChain> = {
         "0xc168343c791d56dd1da4b4b8b0cc1c1ec1a16e6b": "cp0x",
         "0x3b56538833fc02f4f0e75609390f26ded0c32e42": "Re7",
         "0x7a133fbd01736fd076158307c9476cc3877f1af5": "Invariant Group",
+      },
+      testMarketConfigurators: {
         "0x09d8305F49374AEA6A78aF6C996df2913e8f3b19": "Tulipa",
       },
-      testMarketConfigurators: {},
       isPublic: true,
       wellKnownToken: {
         address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
