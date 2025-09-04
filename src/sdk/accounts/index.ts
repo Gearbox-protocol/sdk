0f220f3c9554909d39cf493562e9cfa8a7927d6a
doomsower <12031673+doomsower@users.noreply.github.com>
2025-09-04 08:43:34 -0400

    fix: export ca cervices

--- src/sdk/accounts/index.ts
+++ src/sdk/accounts/index.ts
@@ -1,3 +1,6 @@
+export * from "./AbstractCreditAccountsService.js";
+export * from "./CreditAccountsServiceV300.js";
+export * from "./CreditAccountsServiceV310.js";
 export * from "./constants.js";
 export * from "./createCreditAccountService.js";
 export * from "./types.js";
