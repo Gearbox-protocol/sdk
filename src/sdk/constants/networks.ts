03639046052d77fa2192471e5c774bb601e19e29
doomsower <12031673+doomsower@users.noreply.github.com>
2025-09-09 21:52:09 -0300

    fix: some safety checks for BLOCK_DURATION

--- src/sdk/constants/networks.ts
+++ src/sdk/constants/networks.ts
@@ -23,7 +23,25 @@ export const ADDRESS_PROVIDER_BLOCK: Record<NetworkType, bigint> = {
   Lisk: 18934260n, // arbitrary not deployed yet
 };
 
-const BLOCK_DURATION_LOCAL: PartialRecord<NetworkType, number> = {};
+// local constants are preserved for compatibility with older viem versions
+// values are in milliseconds
+const BLOCK_DURATION_LOCAL: PartialRecord<NetworkType, number> = {
+  Mainnet: 12050,
+  Arbitrum: 260,
+  Optimism: 2000,
+  Base: 2010,
+  Sonic: 910,
+  // New networks
+  MegaETH: 10, // <10ms/block, on testnet
+  Monad: 1000, // on testnet
+  Berachain: 1900,
+  Avalanche: 1700,
+  BNB: 3000,
+  WorldChain: 2000,
+  Etherlink: 1000,
+  Hemi: 12000,
+  Lisk: 2000,
+};
 
 const BLOCK_DURATION = Object.values(CHAINS).reduce<
   Record<NetworkType, number>
@@ -44,7 +62,9 @@ const RAMP_TIME = 30 * 24 * 60 * 60 * 1.2;
 export const RAMP_DURATION_BY_NETWORK: Record<NetworkType, bigint> =
   TypedObjectUtils.entries(BLOCK_DURATION).reduce<Record<NetworkType, bigint>>(
     (acc, [n, duration]) => {
-      acc[n] = BigInt(Math.floor(RAMP_TIME / duration));
+      if (duration !== 0) {
+        acc[n] = BigInt(Math.floor(RAMP_TIME / duration));
+      }
       return acc;
     },
     {} as Record<NetworkType, bigint>,
