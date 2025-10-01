0c5ed6bda0b9bc03d7358149e62c7143a267ec79
doomsower <12031673+doomsower@users.noreply.github.com>
2025-10-01 15:50:19 -0300

    fix: catch in dynamic import

--- src/dev/mint/DealMinter.ts
+++ src/dev/mint/DealMinter.ts
@@ -14,14 +14,17 @@ export class DealMinter extends AbstractMinter implements IMinter {
     dest: Address,
     amount: bigint,
   ): Promise<void> {
+    // Dynamic import of viem-deal to avoid affecting builds when not used
+    const { deal } = await import("viem-deal").catch(() => ({ deal: null }));
+    if (!deal) {
+      throw new Error("peer dependency viem-deal is not installed");
+    }
     // deal explicitly sets balance, so we need to account for what we already have
     let balance = await this.balanceOf(token, dest);
     balance += amount;
 
     this.logger?.debug(`set balance of ${dest} to ${this.fmt(token, balance)}`);
 
-    // Dynamic import of viem-deal to avoid affecting builds when not used
-    const { deal } = await import("viem-deal");
     await deal(this.anvil as unknown as TestClient, {
       erc20: token,
       account: dest,
