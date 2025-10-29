7cf39dd86f8e138707f62aaed17c20b3d81ece1f
esser <blizzz93@gmail.com>
2025-10-29 23:09:52 +0700

    fix: repay

--- src/sdk/accounts/CreditAccountsServiceV310.ts
+++ src/sdk/accounts/CreditAccountsServiceV310.ts
@@ -102,16 +102,21 @@ export class CreditAccountServiceV310
     creditAccount: ca,
     permits,
     to,
+    tokensToClaim,
   }: RepayCreditAccountProps): Promise<CreditAccountOperationResult> {
     const cm = this.sdk.marketRegister.findCreditManager(ca.creditManager);
 
     const addCollateral = collateralAssets.filter(a => a.balance > 0);
 
     const router = this.sdk.routerFor(ca);
+    // HERE
+
     const claimPath = await router.findClaimAllRewards({
       calls: [],
+      tokensToClaim,
       creditAccount: ca,
     });
+    console.log("claimPath", this.sdk.parseMultiCall(claimPath.calls));
 
     const priceUpdates = await this.getPriceUpdatesForFacade({
       creditManager: ca.creditManager,
@@ -146,14 +151,19 @@ export class CreditAccountServiceV310
     creditAccount: ca,
     permits,
     to,
+    tokensToClaim,
   }: RepayAndLiquidateCreditAccountProps): Promise<CreditAccountOperationResult> {
     const cm = this.sdk.marketRegister.findCreditManager(ca.creditManager);
 
     const router = this.sdk.routerFor(ca);
+    // HERE
     const claimPath = await router.findClaimAllRewards({
       calls: [],
+      tokensToClaim,
       creditAccount: ca,
     });
+
+    console.log("claimPath", this.sdk.parseMultiCall(claimPath.calls));
 
     const priceUpdates = await this.getPriceUpdatesForFacade({
       creditManager: ca.creditManager,
@@ -188,12 +198,14 @@ export class CreditAccountServiceV310
 
     minQuota,
     averageQuota,
+    tokensToClaim,
   }: ClaimFarmRewardsProps): Promise<CreditAccountOperationResult> {
     const cm = this.sdk.marketRegister.findCreditManager(ca.creditManager);
 
     const router = this.sdk.routerFor(ca);
     const claimPath = await router.findClaimAllRewards({
       calls: legacyCalls,
+      tokensToClaim,
       creditAccount: ca,
     });
     if (claimPath.calls.length === 0) throw new Error("No path to execute");
