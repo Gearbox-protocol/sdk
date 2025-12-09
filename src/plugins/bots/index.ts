dd99de17a00dcbf51709b1b94ee0dc9f93299066
doomsower <12031673+doomsower@users.noreply.github.com>
2025-12-09 11:33:32 -0300

    fix: export partial liquidation bot v 310

--- src/plugins/bots/index.ts
+++ src/plugins/bots/index.ts
@@ -1,4 +1,5 @@
 export * from "./abi/index.js";
 export * from "./BotsPlugin.js";
 export * from "./PartialLiquidationBotV300Contract.js";
+export * from "./PartialLiquidationBotV310Contract.js";
 export * from "./types.js";
