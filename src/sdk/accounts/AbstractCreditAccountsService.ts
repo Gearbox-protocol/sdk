9e1d49a628abd1a36a2057d41102fcbe8e9150db
esser <blizzz93@gmail.com>
2025-12-16 16:47:16 +0700

    fix: address

--- src/sdk/accounts/AbstractCreditAccountsService.ts
+++ src/sdk/accounts/AbstractCreditAccountsService.ts
@@ -75,11 +75,9 @@ export interface CreditAccountServiceOptions {
   batchSize?: number;
 }
 
-export function getWithdrawalCompressorAddress(chainId: number) {
+export function getWithdrawalCompressorAddress(_chainId: number) {
   // TODO: HARDCODED
-  const compressor =
-    chainId === 1 ? "0xfB79b6713fe214B8748ED7b0db1f93E4f1aC9d29" : undefined;
-  return compressor;
+  return "0x36F3d0Bb73CBC2E94fE24dF0f26a689409cF9023";
 }
 
 export abstract class AbstractCreditAccountService extends SDKConstruct {
