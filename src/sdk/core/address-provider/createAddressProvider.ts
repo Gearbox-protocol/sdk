1eb447fa9d984b1db043ddc1502374ede2d337c1
doomsower <12031673+doomsower@users.noreply.github.com>
2025-12-24 12:53:27 -0300

    fix: skip usually unnecessary version request

--- src/sdk/core/address-provider/createAddressProvider.ts
+++ src/sdk/core/address-provider/createAddressProvider.ts
@@ -1,8 +1,13 @@
 import type { Address } from "viem";
 
 import { iVersionAbi } from "../../../abi/iVersion.js";
-import { isV300, isV310 } from "../../constants/index.js";
+import {
+  ADDRESS_PROVIDER_V310,
+  isV300,
+  isV310,
+} from "../../constants/index.js";
 import type { GearboxSDK } from "../../GearboxSDK.js";
+import { hexEq } from "../../utils/hex.js";
 import { AddressProviderV300Contract } from "./AddressProviderV300Contract.js";
 import { AddressProviderV310Contract } from "./AddressProviderV310Contract.js";
 import type {
@@ -14,11 +19,16 @@ export async function createAddressProvider(
   sdk: GearboxSDK,
   address: Address,
 ): Promise<IAddressProviderContract> {
-  const v = await sdk.client.readContract({
-    address,
-    abi: iVersionAbi,
-    functionName: "version",
-  });
+  let v: bigint;
+  if (hexEq(address, ADDRESS_PROVIDER_V310)) {
+    v = 310n;
+  } else {
+    v = await sdk.client.readContract({
+      address,
+      abi: iVersionAbi,
+      functionName: "version",
+    });
+  }
   return newAddressProvider(sdk, address, Number(v));
 }
 
