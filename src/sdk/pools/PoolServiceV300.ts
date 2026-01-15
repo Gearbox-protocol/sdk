9ce6e84e40fe06e1a50ba3e0c9b0b2405ad4d726
esser <blizzz93@gmail.com>
2026-01-15 20:20:47 +0700

    feat: add pool service

---
+++ src/sdk/pools/PoolServiceV300.ts
@@ -0,0 +1,6 @@
+import { AbstractPoolService } from "./AbstractPoolService.js";
+import type { IPoolsService } from "./types.js";
+
+export class PoolServiceV300
+  extends AbstractPoolService
+  implements IPoolsService {}
