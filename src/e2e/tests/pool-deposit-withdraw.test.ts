da93d554d0d0ad49ca7f7098ad36ebdd6f3a9635
essserrr <blizzz93@gmail.com>
2026-04-13 18:24:28 +0800

    Revert "fix: temporary skip failing tests"
    This reverts commit 723993537c4b8411453ada06721e71c732a1c6a0.

--- src/e2e/tests/pool-deposit-withdraw.test.ts
+++ src/e2e/tests/pool-deposit-withdraw.test.ts
@@ -70,7 +70,7 @@ function encodePoolCall(call: PoolServiceCall): {
   };
 }
 
-describe.skip("pool deposit and withdraw", () => {
+describe("pool deposit and withdraw", () => {
   let sdk: GearboxSDK;
 
   useFixture({ network: "Mainnet", block: BLOCK });
