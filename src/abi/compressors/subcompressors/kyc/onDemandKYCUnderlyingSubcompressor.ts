516d2ebcb282121a0aeea7c48d8fa3138544b4a7
Arkhip Vouba <80775286+57Ark@users.noreply.github.com>
2026-05-01 13:56:40 +0200

    fix: add kyc compressor and subcompressors abi

---
+++ src/abi/compressors/subcompressors/kyc/onDemandKYCUnderlyingSubcompressor.ts
@@ -0,0 +1,33 @@
+export const onDemandKYCUnderlyingSubcompressorAbi = [
+  {
+    type: "function",
+    name: "contractType",
+    inputs: [],
+    outputs: [{ name: "", type: "bytes32", internalType: "bytes32" }],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "getCompressedType",
+    inputs: [],
+    outputs: [
+      { name: "", type: "bytes32", internalType: "bytes32" },
+      { name: "", type: "bytes32", internalType: "bytes32" },
+    ],
+    stateMutability: "pure",
+  },
+  {
+    type: "function",
+    name: "getUnderlyingData",
+    inputs: [{ name: "underlying", type: "address", internalType: "address" }],
+    outputs: [{ name: "", type: "bytes", internalType: "bytes" }],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "version",
+    inputs: [],
+    outputs: [{ name: "", type: "uint256", internalType: "uint256" }],
+    stateMutability: "view",
+  },
+] as const;
