146eeda6eb319c267a574c6a358aff8cfae134e9
Arkhip Vouba <80775286+57Ark@users.noreply.github.com>
2026-07-14 19:13:39 +0200

    fix: update mellow wsc

--- src/abi/compressors/subcompressors/withdrawal/mellowWithdrawalSubcompressor.ts
+++ src/abi/compressors/subcompressors/withdrawal/mellowWithdrawalSubcompressor.ts
@@ -74,6 +74,7 @@ export const mellowWithdrawalSubcompressorAbi = [
             ],
           },
           { name: "claimableAt", type: "uint256", internalType: "uint256" },
+          { name: "extraData", type: "bytes", internalType: "bytes" },
         ],
       },
     ],
